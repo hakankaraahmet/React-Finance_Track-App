4d39a8a9ac466c21b428d30518a6a811d9d08c01
hakankaraahmet <karaahmethkn@gmail.com>
2021-09-02 15:48:21 +0300

    modified app.js index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React,{useState,useEffect} from 'react'
+import Header from "./Components/Header"
+import IncomeForm from './Components/IncomeForm'
+const App = () => {
+  const[income,setIncome] = useState([])
+  const[totalIncome,setTotalIncom] = useState(0)
+  const[expense,setExpense] = useState([])
+  const[totalExpense,setTotalExpense] = useState(0)
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header totalIncome={totalIncome} totalExpense={totalExpense}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
