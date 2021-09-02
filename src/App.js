5d6a37107fd39ae93b9323d7e3ef20e1c788c355
hakankaraahmet <karaahmethkn@gmail.com>
2021-09-02 17:43:37 +0300

    modified index.css and app.js with new items

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import React,{useState,useEffect} from 'react'
 import Header from "./Components/Header"
+import Header2 from "./Components/Header2"
 import IncomeForm from './Components/IncomeForm'
+import ExpenseForm from './Components/ExpenseForm'
+import IncomeList from './Components/IncomeList'
+import ExpenseList from './Components/ExpenseList'
 const App = () => {
   const[income,setIncome] = useState([])
-  const[totalIncome,setTotalIncom] = useState(0)
+  const[totalIncome,setTotalIncome] = useState(0)
   const[expense,setExpense] = useState([])
   const[totalExpense,setTotalExpense] = useState(0)
+
+  useEffect(() => {
+    let totalIncome = 0;
+    let totalExpense = 0;
+
+    for(let i = 0; i < income.length; i++){
+      totalIncome += parseInt(income[i].price)
+    }
+    setTotalIncome(totalIncome)
+
+    for(let i = 0; i < expense.length; i++){
+      totalExpense += parseInt(expense[i].price)
+    }
+    setTotalExpense(totalExpense)
+
+  }, [income,expense])
  
+
+
   return (
     <div>
-      <Header totalIncome={totalIncome} totalExpense={totalExpense}/>
+      <Header totalIncome={totalIncome} />
+      <IncomeForm income = {income} setIncome={setIncome} />
+      <IncomeList income = {income} setIncome={setIncome}/>
+      <hr/>
+      <Header2 totalExpense={totalExpense}/>
+      <ExpenseForm expense={expense} setExpense = {setExpense}/>
+      <ExpenseList expense = {expense} setExpense={setExpense}/>
     </div>
   )
 }
